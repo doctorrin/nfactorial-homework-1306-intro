d173cca369defc2dbe4637f513286a3c6d452b6b
Rin <zhakupova.zarina01@gmail.com>
2022-06-15 00:15:27 +0600

    13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+import a from "./images/a.jpg"
+import b from "./images/b.jpg"
+import c from "./images/c.jpg"
+import e from "./images/d.jpg"
+import d from "./images/e.jpg"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='image'>
+          <h1 className='text'>HEY</h1>
+          <img className='img' src={a}/>
+        </div>
+        <div className='image'>
+          <h1 className='text'>LET'S</h1>
+          <img className='img' src={b}/>
+        </div>
+        <div className='image'>
+          <h1 className='text'>GIVE</h1>
+          <img className='img' src={c}/>
+        </div>
+        <div className='image'>
+          <h1 className='text'>ALL</h1>
+          <img className='img' src={d}/>
+        </div>
+        <div className='image'>
+          <h1 className='text'>YOU CAN</h1>
+          <img className='img' src={e} />
+        </div>
+      </div>
     </div>
   );
 }
